1579ef86a0d40df0a7847e9eab73d0f9de30f00c
priyanshi1399 <priyanshitomar001@gmail.com>
2025-09-22 02:06:49 +0530

    Starting project

---
+++ src/test/java/com/priyanshi/EmployeeManagementCRUD/EmployeeManagementCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyanshi.EmployeeManagementCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
